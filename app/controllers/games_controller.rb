fa6ee46368aa534b5918eaafff7e01125d8fad12
Danilo Raphael Condo <daniloraphaelcondo@gmail.com>
2018-07-25 19:53:22 -0300

    Prepare field for new models

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,9 +5,21 @@ class GamesController < ApplicationController
   end
 
   def new
+    @game = Game.new
+  end
+
+  def create
+    @game = Game.new(game_params)
+    @game.save
+    redirect_to @game
   end
 
   def edit
+  end
+
+  def update
+    @game.update(game_params)
+    redirect_to @game
   end
 
   def show
@@ -18,4 +30,9 @@ class GamesController < ApplicationController
   def set_game
     @game = Game.find(params[:id])
   end
+
+  def game_params
+    params.require(:game).permit(:name, :genre, :image_url, :description, :price, :rating)
+  end
+
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -1,2 +1,13 @@
 class Game < ApplicationRecord
+  validates :name, uniqueness: true, presence: true
+  validates :address, presence: true
+  validates :genre, presence: true
+  # validates :category, inclusion: { in: ['chinese', 'italian', 'french', 'belgian', 'japanese'] }
 end
+
+  # <%= f.input :name %>
+  # <%= f.input :genre %>
+  # <%= f.input :image_url %>
+  # <%= f.input :description %>
+  # <%= f.input :price %>
+  # <%= f.input :rating %>
